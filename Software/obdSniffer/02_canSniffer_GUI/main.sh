a38177459cb154fe60be0ee7a9ee079780110d36
YfNk <quent1vass63@gmail.com>
2023-07-16 22:10:01 +0200

    obdSniffer pictures

--- Software/obdSniffer/02_canSniffer_GUI/main.sh
+++ Software/obdSniffer/02_canSniffer_GUI/main.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source venv/bin/activate
-export PYTHONPATH="/mnt/Projects/piJag/canDrive/02_canSniffer_GUI/venv/lib/python3.9/site-packages"
+export PYTHONPATH="/mnt/Projects/piJag/piJag/Software/obdSniffer/02_canSniffer_GUI/venv/lib/python3.9/site-packages"
 python3 main.py
